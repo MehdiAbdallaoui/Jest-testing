c9872a192f5eb94a3bb64932df0cc1d0cd7598c1
Mehdi ABDALLAOUI <mehdi.abdallaoui@ensiie.fr>
2019-09-27 10:44:06 +0200

    Ajout de la fonction includes ainsi que son test

--- src/interval.test.js
+++ src/interval.test.js
@@ -21,6 +21,25 @@ describe('overlaps', function () {
 	});
 });
 
+describe('includes', function () {
 
+	test('Test includes de [2;7] et [3;5]', () => {
+		var interval_1 =  new Interval(2, 7);
+		var interval_2 =  new Interval(3, 5);
+    		expect(interval_1.includes(interval_2)).toBe(true);
+	});
+
+	test('Test includes de [100;200] et [100;200]', () => {
+		var interval_1 =  new Interval(100, 200);
+		var interval_2 =  new Interval(100, 200);
+    		expect(interval_1.includes(interval_2)).toBe(false);
+	});
+
+	test('Test includes de [60;320] et [61;320]', () => {
+		var interval_1 =  new Interval(60, 320);
+		var interval_2 =  new Interval(61, 320);
+    		expect(interval_1.includes(interval_2)).toBe(false);
+	});
+});
 
 
